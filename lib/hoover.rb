948940fd8ddb88047dd2f2f5dd9f6d639af56889
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-07-09 16:18:56 +0100

    Raise error if outside room

--- lib/hoover.rb
+++ lib/hoover.rb
@@ -1,11 +1,15 @@
 # frozen_string_literal: true
 
+require_relative 'room'
+
 class Hoover
   attr_reader :position_x, :position_y
 
-  def initialize(position_x, position_y)
+  def initialize(position_x, position_y, room_x, room_y)
+    out_of_range if position_x > room_x || position_y > room_y
     @position_x = position_x
     @position_y = position_y
+    @room = Room.new(room_x, room_y)
   end
 
   def move(direction)
@@ -19,5 +23,11 @@ class Hoover
     when 'W'
       @position_x -= 1
     end
+  end
+
+  private
+
+  def out_of_range
+    raise 'Out of range! Please try again.'
   end
 end
