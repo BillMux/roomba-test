58fa489262b23447f1df287e4db308f83fb13f54
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-07-10 14:21:46 +0100

    Return position and room details on func

--- lib/hoover.rb
+++ lib/hoover.rb
@@ -3,18 +3,23 @@
 require_relative 'room'
 
 class Hoover
-  attr_reader :position_x, :position_y, :room
+  attr_reader :position_x, :position_y, :room, :position
 
   def initialize(position_x, position_y, room_x, room_y)
     out_of_range if position_x > room_x || position_y > room_y
     @position_x = position_x
     @position_y = position_y
     @room = Room.new(room_x, room_y)
+    @room_dim = "#{room_x}x#{room_y}"
+    @position = "(#{@position_x}, #{@position_y})"
     clean
   end
 
+  def return_position_and_room
+    "Position: #{@position}\nRoom: #{@room_dim}"
+  end
+
   def move(direction)
-    clean
     case direction
     when 'N'
       @position_y += 1
@@ -26,15 +31,17 @@ class Hoover
       @position_x -= 1
     end
     clean
+    "Hoover has moved to (#{@position_x}, #{@position_y})"
   end
 
   private
 
   def clean
-    p @position_x, @position_y
-    p @room.floor[@position_x][@position_y].dirty?
-    @room.floor[@position_x][@position_y].dirty = false
-    p @room.floor[@position_x][@position_y].dirty?
+    patch = @room.floor[@position_x][@position_y]
+    if patch.dirty?
+      patch.dirty = false
+      "Patch (#{@position_x}, #{@position_y}) has been cleaned"
+    end
   end
 
   def out_of_range
